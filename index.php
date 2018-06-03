cff35f81cee0616ac6aa91eada788ead06e8543f
Surplus_Up (2DU) <min08101@naver.com>
2018-06-04 01:42:56 +0900

    엄청난 진전

--- index.php
+++ index.php
@@ -1,25 +1,85 @@
 <?php
+    $lang_file = array(
+        'insert' => 'Insert',
+        'login' => 'Login',
+        'edit' => 'Edit',
+        'record' => 'Record',
+    );
+
     function html_load($data = '404 Error', $head = '') {
-        $main_html =    '<html>
+        $main_html =    '<!DOCTYPE html>
+                        <html>
                             <head>
+                                <meta charset="utf-8">
+                                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                                <meta name="viewport" content="width=device-width, initial-scale=1">
+                                <title>Test</title>
+                                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+                                <style>
+                                    body {
+                                        padding-top: 50px;
+                                    }
+
+                                    .starter-template {
+                                        padding-top: 20px;
+                                    }
+                                </style>
                                 '.$head.'
                             </head>
                             <body>
-                                <div id="main">
-                                    '.$data.'
+                                <nav class="navbar navbar-inverse navbar-fixed-top">
+                                    <div class="container">
+                                        <div class="navbar-header">
+                                            <a class="navbar-brand" href="'.url_fix().'">Test</a>
+                                        </div>
+                                        <ul class="nav navbar-nav">
+                                            <li><a href="'.url_fix('?action=list').'">'.load_lang('record').'</a></li>
+                                        </ul>
+                                    </div>
+                                </nav>
+                                <div class="container">
+                                    <div class="starter-template">
+                                        '.$data.'
+                                    </div>
                                 </div>
                             </body>
                         </html>';
+
+        $main_html = preg_replace('/\n +/', "\n", $main_html);
                     
         return $main_html;
     }
 
     function redirect($url = '') {
-        if($url = '') {
-            $url = $_SERVER['REQUEST_URI'];
-        }
+        return '<meta http-equiv="refresh" content="0; url='.$_SERVER['PHP_SELF'].$url.'">';
+    }
 
-        return '<meta http-equiv="refresh" content="0; url='.$url.'">';
+    function url_fix($url = '') {
+        return $_SERVER['PHP_SELF'].$url;
+    }
+
+    function render($data) {
+        $data = preg_replace('/\n/', '<br>', $data);
+
+        $data = preg_replace('/\*\*((?:(?!\*\*).)+)\*\*/', '<b>$1</b>', $data);
+        $data = preg_replace('/\*((?:(?!\*).)+)\*/', '<i>$1</i>', $data);
+
+        $data = preg_replace('/__((?:(?!__).)+)__/', '<u>$1</u>', $data);
+        $data = preg_replace('/_((?:(?!_).)+)_/', '<i>$1</i>', $data);
+
+        $data = preg_replace('/~~((?:(?!~~).)+)~~/', '<s>$1</s>', $data);
+
+        return $data;
+    }
+
+    function load_lang($data) {
+        global $lang_file;
+
+        if($lang_file[$data]) {
+            return $lang_file[$data];
+        } else {
+            return 'Error';
+        }
     }
 
     $conn = new PDO('sqlite:data.db');
@@ -37,8 +97,8 @@
     if(!$user_data) {
         if($_SERVER['REQUEST_METHOD'] != 'POST') {
             echo html_load( '<form method="post">
-                                <input name="pw">
-                                <button>InSert</button>
+                                <input name="pw" type="password">
+                                <button>'.load_lang('insert').'</button>
                             </form>');
         } else {
             $insert = $conn -> prepare('insert into set_data (id, data) values ("pw", :pw)');
@@ -51,8 +111,8 @@
         if(!isset($_SESSION['pw'])) {
             if($_SERVER['REQUEST_METHOD'] != 'POST') {
                 echo html_load( '<form method="post">
-                                    <input name="pw">
-                                    <button>Login</button>
+                                    <input name="pw" type="password">
+                                    <button>'.load_lang('login').'</button>
                                 </form>');
             } else {
                 if($user_data[0]['data'] == $_POST["pw"]) {
@@ -62,9 +122,9 @@
                 echo redirect();
             }
         } else {
-            switch($_GET["action"]) {
+            switch($_GET['action']) {
                 case '':
-                    $select = $conn -> query('select data from data where date = "'.date("Y-m-d").'"');
+                    $select = $conn -> query('select data from data where date = "'.date('Y-m-d').'"');
                     $select -> execute();
                     $daily_data = $select -> fetchAll();
                     if(!$daily_data) {
@@ -72,26 +132,82 @@
                             echo html_load( '<form method="post">
                                                 <textarea rows="25" cols="100" name="data"></textarea>
                                                 <br>
-                                                <button>InSert</button>
+                                                <button>'.load_lang('insert').'</button>
                                             </form>');
                         } else {
-                            $insert = $conn -> prepare('insert into data (data, date) values (:data, "'.date("Y-m-d").'")');
-                            $insert -> bindParam(':data', $_POST["data"]);
+                            $insert = $conn -> prepare('insert into data (data, date) values (:data, "'.date('Y-m-d').'")');
+                            $insert -> bindParam(':data', $_POST['data']);
                             $insert -> execute();
 
                             echo redirect();
                         }
                     } else {
-                        echo html_load( date("Y-m-d").'
+                        echo html_load( '<h1>'.date('Y-m-d').'</h1> <a href="'.url_fix('?action=edit').'">('.load_lang('edit').')</a>
+                                        <br>
                                         <br>'
-                                        .preg_replace('/\n/', '<br>', $daily_data[0]['data']));
+                                        .render($daily_data[0]['data']));
                     }
                     
                     break;
-                case 'views':
-                    echo 'test';
+                case 'edit':
+                    if($_SERVER['REQUEST_METHOD'] != 'POST') {
+                        $select = $conn -> query('select data from data where date = "'.date('Y-m-d').'"');
+                        $select -> execute();
+                        $daily_data = $select -> fetchAll();
+                        if($daily_data) {
+                            echo html_load( '<form method="post">
+                                                <textarea rows="25" cols="100" name="data">'.$daily_data[0]['data'].'</textarea>
+                                                <br>
+                                                <button>'.load_lang('insert').'</button>
+                                            </form>');
+                        } else {
+                            echo redirect();
+                        }
+                    } else {
+                        $insert = $conn -> prepare('update data set data = :data where date = "'.date('Y-m-d').'"');
+                        $insert -> bindParam(':data', $_POST['data']);
+                        $insert -> execute();
+
+                        echo redirect();
+                    }
+
+                    break;
+                case 'list':
+                    $select = $conn -> query('select date from data');
+                    $select -> execute();
+                    $all_data = $select -> fetchAll();
+                    if($all_data) {
+                        $list = '';
+                        foreach ($all_data as &$data) {
+                            $list = $list.'<li><a href="'.url_fix('?action=view&date='.$data['date']).'">'.$data['date'].'</a></li>';
+                        }
+
+                        echo html_load($list);
+                    } else {
+                        echo redirect();
+                    }
+
+                    break;
+                case 'view':
+                    if($_GET['date']) {
+                        $select = $conn -> query('select data from data where date = "'.date('Y-m-d', strtotime($_GET['date'])).'"');
+                        $select -> execute();
+                        $daily_data = $select -> fetchAll();
+                        if($daily_data) {
+                            echo html_load( '<h1>'.date('Y-m-d', strtotime($_GET['date'])).'</h1>
+                                            <br>
+                                            <br>'
+                                            .render($daily_data[0]['data']));
+                        } else {
+                            echo redirect();
+                        }
+                    } else {
+                        echo redirect();
+                    }
+
+                    break;
                 default:
-                    echo html_load();
+                    echo redirect();
             }
         }
     }
